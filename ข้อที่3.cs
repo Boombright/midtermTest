909819bc5f803df70d3e6e5e8ef2b780a91f01b6
BoomZa Nightcore <krittameth193@gmail.com>
2021-04-24 15:20:42 +0700

    ข้อที่3

---
+++ ข้อที่3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace midtermTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mode = Console.ReadLine();
+            float filmBackWidth = float.Parse(Console.ReadLine());
+            if (filmBackWidth > 0)
+            {
+                float fLength, fov;
+                if(mode == "fLength")
+                {
+                    fLength = float.Parse(Console.ReadLine());
+                    if(fLength > 0)
+                    {
+                        fov = (float)(2 * Math.Atan(filmBackWidth / (2 * fLength)));
+                        Console.WriteLine("fLegnth = ", fLength);
+                    }
+                }
+                else if(mode == "fov")
+                {
+                    fov = float.Parse(Console.ReadLine());
+                    if(fov > 0.1 && fov < 6.28)
+                    {
+                        fLength = (fLength = (float)(filmBackWidth / (2 * Math.Tan(fov / 2))));
+                        Console.WriteLine("fov = ", fov);
+                    }
+                }
+
+            }
+            else
+                Console.WriteLine("Invalid filmBackWidth. Please input again");
+        }
+    }
+}
+//(int i = 0; i <= height; i++)
+    //(int j = 0; j <= width; j++)
