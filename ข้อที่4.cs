fa12bd40a85dc26759e9c8cc76edef834de9814f
BoomZa Nightcore <krittameth193@gmail.com>
2021-04-24 15:34:49 +0700

    ข้อที่4

---
+++ ข้อที่4.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace midtermTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x, y, dx, dy, step;
+            float x1 = float.Parse(Console.ReadLine());
+            float y1 = float.Parse(Console.ReadLine());
+            float x2 = float.Parse(Console.ReadLine());
+            float y2 = float.Parse(Console.ReadLine());
+            dx = x2 - x1;
+            dy = y2 - y1;
+            int i = 1;
+            if (Math.Abs(dx) >= Math.Abs(dy))
+            {
+                step = Math.Abs(dx);
+                dx /= step;
+                dy /= step;
+                x = x1;
+                y = y1;
+            }
+            else
+                step = Math.Abs(dy);
+                dx /= step;
+                dy /= step;
+                x = x1;
+                y = y1;
+            while (i <= step)
+            {
+                Console.Write(x);
+                Console.Write(",");
+                Console.Write(y);
+                x += dx;
+                y += dy;
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+//(int i = 0; i <= height; i++)
+    //(int j = 0; j <= width; j++)
